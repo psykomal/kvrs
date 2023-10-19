d12762135c5d1bc62b288b7e7ac09a24201abbc5
psykomal <bkomalsai@gmail.com>
2023-10-19 16:22:01 +0530

    project 4 - kvs-server cli

--- src/bin/kvs-server.rs
+++ src/bin/kvs-server.rs
@@ -1,1 +1,40 @@
-fn main() {}
+use std::net::IpAddr;
+
+use ::clap::{Args, Parser, Subcommand};
+use kvs::Result;
+
+#[derive(Parser)]
+#[command(author, version)]
+#[command(about = "KV server")]
+struct Cli {
+    #[command(subcommand)]
+    command: Option<Commands>,
+    #[arg(short, long, default_value = ".")]
+    dir: String,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Start(Start),
+}
+
+#[derive(Args)]
+struct Start {
+    #[arg(short, long, default_value = "127.0.0.1:4000")]
+    addr: IpAddr,
+    #[arg(short, long, default_value = "kvs")]
+    engine: String,
+}
+
+fn main() -> Result<()> {
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Some(Commands::Start(args)) => {
+            // start server
+        }
+        None => {}
+    }
+
+    Ok(())
+}
